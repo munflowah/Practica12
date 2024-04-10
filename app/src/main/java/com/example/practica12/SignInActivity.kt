1302cdc328f6c188d6226d746a42a015685eafd0
Flor Margarita Soto Higuera <101524283+munflowah@users.noreply.github.com>
2024-04-10 15:41:28 -0700

    commit de actividad 12 login con correo

---
+++ app/src/main/java/com/example/practica12/SignInActivity.kt
@@ -0,0 +1,30 @@
+package com.example.practica12
+
+import android.content.ContentValues.TAG
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.practica12.databinding.ActivitySignInBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
+class SignInActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivitySignInBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+
+        binding.welcomeTextView.text = "Holis, sea bienvenido"
+        binding.catImageView.setImageResource(R.drawable.images)
+
+
+    }
+}
